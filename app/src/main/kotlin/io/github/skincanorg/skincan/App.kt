da0a90070a80b954a3fe752a95d943388ecd9586
ziro <palembani@gmail.com>
2022-05-10 11:09:15 +0700

    + Preparation pt.2 * Added splash screen * Use view binding * Use dagger+hilt

--- app/src/main/kotlin/io/github/skincanorg/skincan/App.kt
+++ app/src/main/kotlin/io/github/skincanorg/skincan/App.kt
@@ -1,5 +1,7 @@
 package io.github.skincanorg.skincan
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application()
--- app/src/main/kotlin/io/github/skincanorg/skincan/ui/MainActivity.kt
+++ app/src/main/kotlin/io/github/skincanorg/skincan/ui/MainActivity.kt
@@ -2,11 +2,19 @@ package io.github.skincanorg.skincan.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.github.skincanorg.skincan.R
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
+import io.github.skincanorg.skincan.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
