a6f6754891ee67ce2963ac4cf8d31bcda3f3bb81
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2022-05-19 19:14:23 +0700

    + README and licensing (#2)

---
+++ README.md
@@ -0,0 +1,22 @@
+# SkinCan
+A skin cancer detection app
+
+## MVP (Minimum Viable Product)
+- ~~Onboarding~~
+- Auth (Waiting for backend)
+- Camera
+- Detection Result
+- Result List
+
+## Mobile Team
+- Ahmad Ansori Palembani (a2191f1821 - [@null2264](https://github.com/null2264))
+- Muhammad Fharid Akbar (a7191f1820 - [@FATx64](https://github.com/FATx64))
+
+## License
+```
+Copyright (C) 2022 SkinCan Project
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+```
--- app/src/main/kotlin/io/github/skincanorg/skincan/data/onboarding/OnboardAdapter.kt
+++ app/src/main/kotlin/io/github/skincanorg/skincan/data/onboarding/OnboardAdapter.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2022 SkinCan Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package io.github.skincanorg.skincan.data.onboarding
 
 import android.content.Context
@@ -8,7 +16,7 @@ import androidx.recyclerview.widget.RecyclerView
 import io.github.skincanorg.skincan.databinding.OnboardingItemBinding
 
 class OnboardAdapter(private val context: Context, data: List<OnboardScreen>) : RecyclerView.Adapter<OnboardAdapter.ViewHolder>() {
-    private val actualData = listOf(data.last()) + data + listOf(data.first())
+    private val fakeData = listOf(data.last()) + data + listOf(data.first())
     class ViewHolder(private val context: Context, private val binding: OnboardingItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(screen: OnboardScreen) {
             screen.apply {
@@ -24,8 +32,8 @@ class OnboardAdapter(private val context: Context, data: List<OnboardScreen>) : 
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(actualData[position])
+        holder.bind(fakeData[position])
     }
 
-    override fun getItemCount(): Int = actualData.size
+    override fun getItemCount(): Int = fakeData.size
 }
