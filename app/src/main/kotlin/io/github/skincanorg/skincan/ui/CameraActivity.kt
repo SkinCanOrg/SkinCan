d021107e0a4ed27b9bd4ede659cc7f9b4f7a4b21
FATx64 <muhammadfharidakbar@gmail.com>
2022-05-20 18:38:26 +0700

    add camera stuff

---
+++ app/src/main/kotlin/io/github/skincanorg/skincan/ui/CameraActivity.kt
@@ -0,0 +1,130 @@
+package io.github.skincanorg.skincan.ui
+
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import io.github.skincanorg.skincan.R
+import io.github.skincanorg.skincan.databinding.ActivityCameraBinding
+import io.github.skincanorg.skincan.lib.Util
+import java.io.File
+
+class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+    private var imageCapture: ImageCapture? = null
+    private var getFile: File? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.captureImg.setOnClickListener {
+            imageCapture
+        }
+
+        binding.closeCamera.setOnClickListener {
+            closeCamera()
+        }
+
+        binding.flipCamera.setOnClickListener {
+            cameraSelector =
+                if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+                else CameraSelector.DEFAULT_BACK_CAMERA
+            startCamera()
+        }
+
+        binding.gallery.setOnClickListener {
+            startGallery()
+        }
+
+        binding.torchCamera.setOnClickListener {
+            TODO("add this shit")
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (exc: Exception) {
+                Util.makeToastShort(this,getString(R.string.failed_open_camera))
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    //for open gallery
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val selectedImg: Uri = it.data?.data as Uri
+            val myFile = Util.uriToFile(selectedImg, this@CameraActivity)
+            getFile = myFile
+            //binding.viewFinder.setImageUri(selectedImg)
+        }
+    }
+
+    private fun closeCamera(){
+        Intent(this@CameraActivity, MainActivity::class.java)
+    }
+
+
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+}
