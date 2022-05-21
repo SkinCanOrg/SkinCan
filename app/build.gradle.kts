74faf7490b453cb36dbd88eafadc47898895d47e
FATx64 <73746785+FATx64@users.noreply.github.com>
2022-05-21 08:28:36 +0700

    + Added CameraX (#4)
    * add camera stuff
    
    * add camera stuff & license
    
    * add camera X and desugaring
    
    * fix todo
    
    * change srcCompat with src on layout,
    add marginEnd in every icon,
    add some function and some PS at CameraActivity.kt
    
    Co-authored-by: FATx64 <muhammadfharidakbar@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    //camera X
+    val camerax_version = "1.1.0-beta02"
+    implementation ("androidx.camera:camera-camera2:${camerax_version}")
+    implementation ("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation ("androidx.camera:camera-view:${camerax_version}")
+
+    //desugaring
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.5")
 }
