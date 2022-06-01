0cbb4ffef62f9a28d6e64b4093d80bf921c7379a
ziro <palembani@gmail.com>
2022-06-01 09:06:00 +0700

    + Fix warning

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,3 +5,14 @@ plugins {
 repositories {
     mavenCentral()
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
