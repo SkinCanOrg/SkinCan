7e082111284251b5a757be3f9135eb5b4eccc066
ziro <palembani@gmail.com>
2022-06-01 08:37:35 +0700

    + Adding ML stuff I think it's broken right now, it always return the same value

--- app/src/main/kotlin/io/github/skincanorg/skincan/ui/camera/ScannerActivity.kt
+++ app/src/main/kotlin/io/github/skincanorg/skincan/ui/camera/ScannerActivity.kt
@@ -8,38 +8,61 @@
 
 package io.github.skincanorg.skincan.ui.camera
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
+import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions
+import com.google.firebase.ml.modeldownloader.DownloadType
+import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader
 import io.github.skincanorg.skincan.R
 import io.github.skincanorg.skincan.databinding.ActivityScannerBinding
 import io.github.skincanorg.skincan.lib.Util
+import org.tensorflow.lite.Interpreter
 import java.io.File
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class ScannerActivity : AppCompatActivity() {
     private val binding: ActivityScannerBinding by viewBinding(CreateMethod.INFLATE)
     private var shouldLoop = false
     private var looped = true
+    private val imageSize = 28
+    private var interpreter: Interpreter? = null
+    private lateinit var image: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val conditions = CustomModelDownloadConditions.Builder()
+            .requireWifi()
+            .build()
+        FirebaseModelDownloader.getInstance()
+            .getModel("CancerDetector", DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND, conditions)
+            .addOnSuccessListener { model ->
+                val modelFile = model?.file
+                if (modelFile != null)
+                    interpreter = Interpreter(modelFile)
+            }
+
         binding.apply {
             val photoFile = intent.extras?.get("IMG_FILE") as File?
             if (photoFile != null) {
-                val img = Util.processBitmap(
+                image = Util.processBitmap(
                     BitmapFactory.decodeFile(photoFile.path),
                     photoFile,
                 )
 
                 Glide.with(ivImagePreview)
-                    .load(img)
+                    .load(image)
                     .into(ivImagePreview)
             } else {
                 finish()
@@ -81,11 +104,49 @@ class ScannerActivity : AppCompatActivity() {
             btnScan.setOnClickListener {
                 shouldLoop = true
                 root.transitionToState(R.id.loop_start, 1)
+
+                val resizedImg = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+                classifyImage(resizedImg)
             }
 
             btnCancel.setOnClickListener {
                 finish()
             }
         }
+    }
+
+    private fun classifyImage(bitmap: Bitmap) {
+        // TODO - Keeps giving me 6 (label: df)
+        val numBytesPerChannel = 4 // float
+        val input =
+            ByteBuffer.allocateDirect(1 * imageSize * imageSize * 3 * numBytesPerChannel).order(ByteOrder.nativeOrder())
+        for (y in 0 until imageSize) {
+            for (x in 0 until imageSize) {
+                val px = bitmap.getPixel(x, y)
+
+                // Get channel values from the pixel value.
+                val r = Color.red(px)
+                val g = Color.green(px)
+                val b = Color.blue(px)
+
+                // Not sure, but this should be [0.0, 0.1]?
+                val rf = r / 255f
+                val gf = g / 255f
+                val bf = b / 255f
+
+                input.putFloat(rf)
+                input.putFloat(gf)
+                input.putFloat(bf)
+            }
+        }
+        val bufferSize = 1 * 7 * numBytesPerChannel
+        val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
+        interpreter?.run(input, modelOutput)
+        modelOutput.rewind()
+        val probabilities = modelOutput.asFloatBuffer()
+        for (i in 0 until probabilities.capacity()) {
+            Log.d("ziML", i.toString())
+        }
+        shouldLoop = false
     }
 }
