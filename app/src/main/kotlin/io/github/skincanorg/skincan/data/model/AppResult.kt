4db5f1b95a8e9b59afc6d027c569666821ad7464
ziro <palembani@gmail.com>
2022-06-05 21:24:39 +0700

    + Added missing file

---
+++ app/src/main/kotlin/io/github/skincanorg/skincan/data/model/AppResult.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2022 SkinCan Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package io.github.skincanorg.skincan.data.model
+
+sealed class AppResult<out R> {
+    data class Success<out T>(val data: T) : AppResult<T>()
+    data class Error<T>(val message: String, val data: T? = null) : AppResult<T>()
+    object Loading : AppResult<Nothing>()
+}
